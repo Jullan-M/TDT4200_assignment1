11e82a6f111703409a3732e86663acf2174a87d9
jullan <jmihkkal_lindi@hotmail.com>
2019-08-27 11:07:52 +0200

    Doubling resolution

--- main.c
+++ main.c
@@ -16,8 +16,8 @@ void invert_colors(uchar* image) {
 }
 
 void flip_image(uchar* image) {
-    //      First we make a copy of the image
-    uchar *temp = calloc(XSIZE * YSIZE * 3, 1);
+    // First we make a copy of the image
+    uchar *temp = malloc(XSIZE * YSIZE * 3);
     for (int y = 0; y < YSIZE; y++) {
         for (int x = 0; x < XSIZE; x++) {
             for (int c = 0; c < 3; c++) {
@@ -26,7 +26,7 @@ void flip_image(uchar* image) {
         }
     }
 
-    //      Now the flipping begins
+    // Now the flipping begins
     for (int y = 0; y < YSIZE; y++) {
         for (int x = 0; x < XSIZE; x++) {
             for (int c = 0; c < 3; c++) {
@@ -34,53 +34,49 @@ void flip_image(uchar* image) {
             }
         }
     }
+    free(temp);
 }
 
 void all_blacks_are_now_blue(uchar* image) {
     for (int y = 0; y < YSIZE; y++) {
         for (int x = 0; x < XSIZE; x++) {
-            uchar *r = & image[3 * XSIZE * y + x * 3 + 0];
-            uchar *g = & image[3 * XSIZE * y + x * 3 + 1];
-            uchar *b = & image[3 * XSIZE * y + x * 3 + 2];
+            uchar* r = & image[3 * XSIZE * y + x * 3 + 0];
+            uchar* g = & image[3 * XSIZE * y + x * 3 + 1];
+            uchar* b = & image[3 * XSIZE * y + x * 3 + 2];
 
-            if (*r == 0 && *g == 0 && *b == 0) {
+            if (*r < 10 && *g < 10 && *b < 10) {
                 *r = 255; // Okay, this was supposed to be *b, so I may have misunderstood the indexing here.
             }
         }
     }
 }
 
-void double_image_size(uchar* image) {
-    //      First we make a copy of the image
-    uchar *temp = calloc(XSIZE * YSIZE * 3, 1);
-    for (int y = 0; y < YSIZE; y++) {
-        for (int x = 0; x < XSIZE; x++) {
+void random_rectangle(uchar* image) {
+    for (int y = 200; y < 400; y++) {
+        for (int x = 2000; x < 2500; x++) {
             for (int c = 0; c < 3; c++) {
-                temp[3 * XSIZE * y + x * 3 + c] = image[3 * XSIZE * y + x * 3 + c];
+                image[3 * XSIZE * y + x * 3 + c] = 255;
             }
         }
     }
+}
 
-    // Reallocate the image area in memory.
-    /*
-    temp = realloc(image, 4 * XSIZE  * YSIZE * 3);
-
+void double_image_size(const uchar* image_old, uchar* image_new) {
     // Duplicate each pixel in original image to 2x2 areas in new image.
     for (int y = 0; y < YSIZE; y++) {
         for (int x = 0; x < XSIZE; x++) {
             for (int c = 0; c < 3; c++) {
-                image[3 * 2 * XSIZE * (2*y) + (2*x) * 3 + c] = temp[3 * XSIZE * y + x * 3 + c];
-                image[3 * 2 * XSIZE * (2*y) + (2*x + 1) * 3 + c] = temp[3 * XSIZE * y + x * 3 + c];
-                image[3 * 2 * XSIZE * (2*y + 1) + (2*x) * 3 + c] = temp[3 * XSIZE * y + x * 3 + c];
-                image[3 * 2 * XSIZE * (2*y + 1) + (2*x + 1) * 3 + c] = temp[3 * XSIZE * y + x * 3 + c];
+                image_new[3 * (2 * XSIZE) * (2*y) + (2*x) * 3 + c] = image_old[3 * XSIZE * y + x * 3 + c];
+                image_new[3 * (2 * XSIZE) * (2*y) + (2*x + 1) * 3 + c] = image_old[3 * XSIZE * y + x * 3 + c];
+                image_new[3 * (2 * XSIZE) * (2*y + 1) + (2*x) * 3 + c] = image_old[3 * XSIZE * y + x * 3 + c];
+                image_new[3 * (2 * XSIZE) * (2*y + 1) + (2*x + 1) * 3 + c] = image_old[3 * XSIZE * y + x * 3 + c];
             }
         }
     }
-     */
 }
 
 int main() {
-	uchar *image = calloc(XSIZE * YSIZE * 3, 1); // Three uchars per pixel (RGB)
+	uchar* image = calloc(XSIZE * YSIZE * 3, 1); // Three uchars per pixel (RGB)
 	readbmp("before.bmp", image);
 
 	// Alter the image here
@@ -95,10 +91,12 @@ int main() {
 	all_blacks_are_now_blue(image);
 
 	// Resize image
-	double_image_size(image);
+	//  This requires that we make a new uchar pointer, since we can't change the size of the existing array.
+	uchar* new_image = malloc(4 * XSIZE * YSIZE * 3);
+	double_image_size(image, new_image);
 
-
-	savebmp("after.bmp", image, XSIZE, YSIZE);
+	savebmp("after.bmp", new_image, 2*XSIZE, 2*YSIZE);
 	free(image);
+	free(new_image);
 	return 0;
 }
